62d1b6c58bc8694993f32bae4449f4f3c1a25588
hikarisaotom <clau_cortes10@hotmail.com>
2018-02-09 14:13:08 -0600

    Base

--- P3Lab#3_ClaudiaCortes.cpp
+++ P3Lab#3_ClaudiaCortes.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#include <stdlib.h>
+#include <math.h>
+//Metodos.
 int menu ();
+void Permutaciones();
 int main()
 {
 	int Respuesta=menu();
@@ -13,7 +17,7 @@ int main()
 	       }
 	case 2:{
 	       cout<<"selecciono Permutaciones"<<endl;
-	       
+	       Permutaciones();
 	       break;
 	       }
 	 case 3:{
@@ -34,4 +38,27 @@ int menu (){
 	return Respuesta;
 }//Fin del menu.
 
+void Permutaciones (){
+	string Numero="12345";
+	char * Numeros= NULL;
+	string * Permutaciones = NULL;
+	Permutaciones = new string  [12];
+	Numeros = new char [4];
+	while (Numero.size()!=4){
+		cout << "Ingrese el numero de cuatro digitos";
+		cin >>Numero;
+	}//Fin del while de valiadacion
+       //Nos aseguramos de que los numeros esten
+       //siendo tomados adecuadamente.	
+	cout<< "Primer numero"<<Numero.substr(0, 1)<<endl;
+	cout <<"Segundo numero"<<Numero.substr(1, 1)<<endl;
+	cout <<"Tercer numero "<< Numero.substr(2, 1)<<endl;
+	cout <<"Cuarto numero"<<Numero.substr(3, 3)<<endl;
+	//Llenamos los Arreglos con los numeros.
+/*	Numeros[0]=Numero.substr(0, 1);
+	Numeros[1]=Numero.substr(1, 2);
+	Numeros[2]=Numero.substr(2, 3);
+	NUmeros[3]=Numero.substr(3, 4);*/
+	//Procederemos a agregar los numeros a 
+}//FIn dle metodo que hace permutaciones.
 
